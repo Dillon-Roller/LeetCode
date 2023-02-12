855968f00db963ab01e5421ac65ab95f486c5780
Dillon Roller <dillonroller.18@gmail.com>
2023-02-12 05:18:54 -0500

    updates

--- source/Arrays-And-Strings/sliding_window.cpp
+++ source/Arrays-And-Strings/sliding_window.cpp
@@ -18,9 +18,29 @@ int find_longest_subarray(const vector<int>& v, const int k) {
 
         max_length = max(max_length, right - left + 1);
     }
+
     return max_length;
+}
+
+double find_max_average(const vector<int>& nums, int k) {
+    double sum = 0;
+    double max_sum;
+
+    for(int i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    max_sum = sum;
+    for(int i = k; i < nums.size(); i++) {
+        sum += nums[i] - nums[i - k];
+        max_sum = max(max_sum, sum);
+    }
+    cout << max_sum / k << endl;
+    return max_sum / k;
 }
 
 int main() {
     assert(find_longest_subarray({10, 20, 30}, 50) == 2);
+
+    assert(find_max_average({1,12,-5,-6,50,3}, 4) == 5.0/2);
 }
