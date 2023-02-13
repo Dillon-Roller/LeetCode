69a79714ad0fe98373906cb334b501e8b4473405
Dillon Roller <dillonroller.18@gmail.com>
2023-02-13 03:14:07 -0500

    fix stuff

--- arrays-and-strings.cpp
+++ arrays-and-strings.cpp
@@ -104,6 +104,62 @@ vector<int> sorted_squares(const vector<int>& numbers) {
     return squares;
 }
 
+int find_longest_subarray(const vector<int>& v, const int k) {
+    int left = 0;
+    int sum = 0;
+    int max_length = 0;
+
+    for (int right = 0; right < v.size(); right++) {
+        sum += v[right];
+        while(sum > k) {
+            sum -= v[left];
+            left++;
+        }
+
+        max_length = max(max_length, right - left + 1);
+    }
+
+    return max_length;
+}
+
+double find_max_average(const vector<int>& nums, int k) {
+    double sum = 0;
+    double max_sum;
+
+    for(int i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    max_sum = sum;
+    for(int i = k; i < nums.size(); i++) {
+        sum += nums[i] - nums[i - k];
+        max_sum = max(max_sum, sum);
+    }
+    return max_sum / k;
+}
+
+int longest_ones(const vector<int>& nums, int k) {
+    int left = 0;
+    int right;
+
+    for(right = 0; right < nums.size(); right++) {
+        if(nums[right] == 0) {
+            k--;
+        }
+
+        if (k < 0) {
+            if(nums[left] == 0) {
+                k++;
+            }
+            left++;
+        }
+    }
+
+    cout << right - left << endl;
+    return right - left;
+}
+
+
 int main() {
     assert(is_palindrome("racecar"));
     assert(!is_palindrome("racecars"));
@@ -124,4 +180,10 @@ int main() {
     assert(s == "olleh");
 
     assert(sorted_squares({10, 6, 3, 1}) == vector<int>({100, 36, 9, 1}));
+
+    assert(find_longest_subarray({10, 20, 30}, 50) == 2);
+
+    assert(find_max_average({1,12,-5,-6,50,3}, 4) == 12.75);
+
+    assert(longest_ones({0, 1, 1, 1, 0, 1, 0, 1}, 1) == 5);
 }
