82dda97e8115f749cbbc0f3e400696bab2593c9b
Dillon Roller <dillonroller.18@gmail.com>
2023-02-14 04:11:58 -0500

    prefix

--- arrays-and-strings.cpp
+++ arrays-and-strings.cpp
@@ -159,6 +159,26 @@ int longest_ones(const vector<int>& nums, int k) {
     return right - left;
 }
 
+vector<int> build_prefix(const vector<int>& nums) {
+    vector<int> prefix = {nums[0]};
+    for (int i = 1; i < nums.size(); i++) {
+        prefix.push_back(prefix.back() + nums[i]);
+    }
+}
+
+vector<bool> answer_queries(vector<int>& nums, vector<vector<int>>& queries, int limit) {
+    vector<int> prefix = build_prefix(nums);
+    
+    vector<bool> ans;
+    for (vector<int>& query: queries) {
+        int x = query[0], y = query[1];
+        int curr = prefix[y] - prefix[x] + nums[x];
+        ans.push_back(curr < limit);
+    }
+    
+    return ans;
+}
+
 
 int main() {
     assert(is_palindrome("racecar"));
