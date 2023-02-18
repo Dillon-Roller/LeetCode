3cf3681579912f39c41124772be886838b9cc6dd
Dillon Roller <dillonroller.18@gmail.com>
2023-02-17 22:36:46 -0500

    number of nice subarrays

--- arrays-and-strings.cpp
+++ arrays-and-strings.cpp
@@ -250,6 +250,20 @@ int subarraySum(vector<int>& nums, int k) {
     return ans;
 }
 
+int numberOfSubarrays(vector<int>& nums, int k) {
+    unordered_map<int, int> counts;
+    counts[0] = 1;
+    int ans = 0, curr = 0;
+    
+    for (int num: nums) {
+        curr += num % 2;
+        ans += counts[curr - k];
+        counts[curr] += 1;
+    }
+    
+    return ans;
+}
+
 int main() {
     assert(is_palindrome("racecar"));
     assert(!is_palindrome("racecars"));
