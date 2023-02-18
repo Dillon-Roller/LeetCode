5de560352c05a165ac8bc7735d601b70735dfbf3
Dillon Roller <dillonroller.18@gmail.com>
2023-02-17 22:46:16 -0500

    equalPairs

--- hash_maps.cpp
+++ hash_maps.cpp
@@ -145,3 +145,30 @@ int getDigitSum(int num) {
     
     return digitSum;
 }
+
+int equalPairs(vector<vector<int>>& grid) {
+    // C++ map can take vectors as keys
+    // However, map instead of unordered_map has O(log n)
+    // operations, but it is still much faster than O(n)
+    map<vector<int>, int> dic;
+    for (vector<int> row: grid) {
+        dic[row]++;
+    }
+    
+    map<vector<int>, int> dic2;
+    for (int col = 0; col < grid[0].size(); col++) {
+        vector<int> currentCol;
+        for (int row = 0; row < grid.size(); row++) {
+            currentCol.push_back(grid[row][col]);
+        }
+        
+        dic2[currentCol]++;
+    }
+    
+    int ans = 0;
+    for (auto [arr, val]: dic) {
+        ans += val * dic2[arr];
+    }
+    
+    return ans;
+}
