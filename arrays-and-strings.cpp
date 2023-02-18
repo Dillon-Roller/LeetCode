80ad4df25d10c36932dadb777a37c3c196a826dc
Dillon Roller <dillonroller.18@gmail.com>
2023-02-17 22:27:08 -0500

    findLongestSubstring

--- arrays-and-strings.cpp
+++ arrays-and-strings.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <cassert>
+#include <unordered_map>
 
 using namespace std;
 
@@ -174,6 +175,26 @@ vector<bool> answer_queries(vector<int>& nums, vector<vector<int>>& queries, int
         int x = query[0], y = query[1];
         int curr = prefix[y] - prefix[x] + nums[x];
         ans.push_back(curr < limit);
+    }
+    
+    return ans;
+}
+
+int findLongestSubstring(string s, int k) {
+    unordered_map<char, int> counts;
+    int left = 0, ans = 0;
+    
+    for (int right = 0; right < s.size(); right++) {
+        counts[s[right]]++;
+        while (counts.size() > k) {
+            counts[s[left]]--;
+            if (counts[s[left]] == 0) {
+                counts.erase(s[left]);
+            }
+            left++;
+        }
+        
+        ans = max(ans, right - left + 1);
     }
     
     return ans;
